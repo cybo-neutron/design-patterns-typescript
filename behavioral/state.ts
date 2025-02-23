1e96154be2207b75b9f092ecff79287233fff2ee
neer <neer@email.com>
2025-02-23 14:02:26 +0530

    saving state pattern

--- behavioral/state.ts
+++ behavioral/state.ts
@@ -27,6 +27,94 @@ class IdleState implements State {
     console.log(`Exiting Idle state`);
   }
   handleState() {
-    console.log(``);
+    console.log(`Yo I'm idle.`);
   }
 }
+
+
+class PatrolState implements State {
+  e: EnemyState;
+
+  constructor() {
+    this.e = EnemyState.PATROL;
+  }
+
+  enterState() {
+    console.log(`Entering Patrol state`);
+  }
+  exitState() {
+    console.log(`Exiting Patrol state`);
+  }
+  handleState() {
+    console.log(`Patrolling with full attention`);
+  }
+}
+
+
+class ChaseState implements State {
+  e: EnemyState;
+
+  constructor() {
+    this.e = EnemyState.CHASE;
+  }
+
+  enterState() {
+    console.log(`Entering Chase state`);
+  }
+  exitState() {
+    console.log(`Exiting Chase state`);
+  }
+  handleState() {
+    console.log(`Chasing player`);
+  }
+}
+
+
+class AttackState implements State {
+  e: EnemyState;
+
+  constructor() {
+    this.e = EnemyState.ATTACK;
+  }
+
+  enterState() {
+    console.log(`Entering Attack state`);
+  }
+  exitState() {
+    console.log(`Exiting Attack state`);
+  }
+  handleState() {
+    console.log(`Attacking player`);
+  }
+}
+
+
+class Enemy {
+  currentState: State
+
+  constructor() {
+    this.currentState = new IdleState()
+    this.currentState.enterState()
+  }
+
+  transitionState(targetState: State) {
+    this.currentState.exitState()
+    this.currentState = targetState
+    this.currentState.enterState()
+  }
+}
+
+
+
+const enemy = new Enemy()
+
+// go to patrol state
+enemy.transitionState(new PatrolState())
+// while patroling enemy sees the player -> it starts chasing the player
+enemy.transitionState(new ChaseState())
+// the player is now in th enemy's attack range -> it starts attaking the player
+enemy.transitionState(new AttackState())
+// the player runs away from the enemy -> the enemy goes to idle state
+enemy.transitionState(new IdleState())
+
+
